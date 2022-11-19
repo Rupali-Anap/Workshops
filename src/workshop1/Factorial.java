9c8903e8aa26eb47be97151b35bd5655550c9306
sachin satalkar <sachinsatalkar@gmail.com>
2022-11-19 16:32:20 +0530

    Added: Factorial Program

---
+++ src/workshop1/Factorial.java
@@ -0,0 +1,12 @@
+package workshop1;
+
+public class Factorial {
+    public static void main(String args[]){
+        int i,fact=1;
+        int num=6;
+        for(i=1;i<=num;i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+num+" is: " +fact );
+    }
+}
